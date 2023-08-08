ff3b11ce2ca45967e6a0425dfc178cc5f8975b49
Roman <galyshev.roman@gmail.com>
2023-08-08 11:55:07 +0300

    0.2 button identifications

--- start.py
+++ start.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    title = 'Вітання'
-    return render_template('index.html', title=title)
+    if request.method == 'GET':
+        title = 'Вітання'
+        return render_template('index.html', title=title)
+    else:
+        title = 'Вітання'
+        if request.form['button'] == 'add':
+            print('add')
+        elif request.form['button'] == 'statistics':
+            print('statistics')
+        elif request.form['button'] == 'find':
+            print('find')
+        else:
+            print('new')
+
+        return render_template('index.html', title=title)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
